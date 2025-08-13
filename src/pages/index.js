a675c043cf15ca9c6836ea71c1e53209fc49bed8
Laksh Patil <lakshpatil574@gmail.com>
2025-08-13 22:01:31 +0530

    Delete src directory

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Welcome } from './Welcome';
-export { default as RoleSelection } from './RoleSelection';
-export { default as DonorLogin } from './DonorLogin';
-export { default as PatientLogin } from './PatientLogin';
-export { default as DonorRegister } from './DonorRegister';
-export { default as PatientRegister } from './PatientRegister';
