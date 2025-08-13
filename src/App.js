e3f014104544bab53a6d8520ec456c17cecb2afb
Laksh25-code <lakshpatil574@gmail.com>
2025-08-13 20:28:09 +0530

    Update
    make a Pages folder in src and create a welcome.js and route tin app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import About from './components/About';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Welcome } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header />
-      
-      {/* Hero Section */}
-      <section id="home">
-        <Hero />
-      </section>
-      
-      {/* Features Section */}
-      <section id="services">
-        <Features />
-      </section>
-      
-      {/* About Section */}
-      <section id="about">
-        <About />
-      </section>
-      
-      {/* Footer */}
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
